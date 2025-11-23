1455a2f4458da8a587b29b6d6c6d5fd5becebf51
legacy3 <20936924+legacy3@users.noreply.github.com>
2025-11-24 00:39:35 +0100

    fix: refactor EventSchedulerService to use SchedulerState and improve event handling

--- packages/wowlab-services/src/internal/scheduler/EventSchedulerService.ts
+++ packages/wowlab-services/src/internal/scheduler/EventSchedulerService.ts
@@ -1,50 +1,96 @@
 import * as Events from "@wowlab/core/Events";
+import * as Schemas from "@wowlab/core/Schemas";
 import * as Effect from "effect/Effect";
 import * as PubSub from "effect/PubSub";
 import * as Ref from "effect/Ref";
+import { Map } from "immutable";
 import TinyQueue from "tinyqueue";
+
+interface SchedulerState {
+  counter: number;
+  currentTime: number;
+  index: Map<Schemas.Branded.EventID, Events.ScheduledEvent>;
+  queue: TinyQueue<Events.ScheduledEvent>;
+}
+
+const eventComparator = (
+  a: Events.ScheduledEvent,
+  b: Events.ScheduledEvent,
+): number => {
+  if (a.time !== b.time) {
+    return a.time - b.time;
+  }
+
+  if (a.priority !== b.priority) {
+    return b.priority - a.priority;
+  }
+
+  return a.id.localeCompare(b.id);
+};
+
+const createEmptyState = (): SchedulerState => ({
+  counter: 0,
+  currentTime: 0,
+  index: Map<Schemas.Branded.EventID, Events.ScheduledEvent>(),
+  queue: new TinyQueue<Events.ScheduledEvent>([], eventComparator),
+});
 
 export class EventSchedulerService extends Effect.Service<EventSchedulerService>()(
   "EventSchedulerService",
   {
     effect: Effect.gen(function* () {
-      const queueRef = yield* Ref.make(
-        new TinyQueue<Events.SimulationEvent>([], (a, b) => a.time - b.time),
-      );
+      const stateRef = yield* Ref.make<SchedulerState>(createEmptyState());
 
       // Create PubSub for event stream
-      const eventPubSub = yield* PubSub.unbounded<Events.SimulationEvent>();
+      const eventPubSub = yield* PubSub.unbounded<Events.ScheduledEvent>();
 
       return {
-        clear: () =>
-          Ref.set(
-            queueRef,
-            new TinyQueue<Events.SimulationEvent>(
-              [],
-              (a, b) => a.time - b.time,
-            ),
-          ),
+        clear: () => Ref.set(stateRef, createEmptyState()),
 
         dequeue: () =>
-          Ref.modify(queueRef, (queue) => {
-            const event = queue.pop();
-            return [event, queue] as const;
+          Ref.modify(stateRef, (state) => {
+            const event = state.queue.pop();
+            if (!event) {
+              return [undefined, state] as const;
+            }
+
+            return [
+              event,
+              {
+                ...state,
+                currentTime: event.time,
+                index: state.index.delete(event.id),
+              },
+            ] as const;
           }),
 
         events: eventPubSub,
 
-        peek: () => Ref.get(queueRef).pipe(Effect.map((queue) => queue.peek())),
+        isEmpty: () =>
+          Ref.get(stateRef).pipe(
+            Effect.map((state) => state.queue.length === 0),
+          ),
 
-        schedule: (event: Events.SimulationEvent) =>
+        peek: () =>
+          Ref.get(stateRef).pipe(Effect.map((state) => state.queue.peek())),
+
+        schedule: (event: Events.ScheduledEvent) =>
           Effect.gen(function* () {
-            yield* Ref.update(queueRef, (queue) => {
-              queue.push(event);
-              return queue;
+            yield* Ref.update(stateRef, (state) => {
+              state.queue.push(event);
+
+              return {
+                ...state,
+                index: state.index.set(event.id, event),
+              };
             });
 
             // Publish event to stream for observers
             yield* PubSub.publish(eventPubSub, event);
           }),
+
+        size: () =>
+          Ref.get(stateRef).pipe(Effect.map((state) => state.queue.length)),
       };
     }),
   },
