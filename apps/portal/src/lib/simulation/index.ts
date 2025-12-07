d4292471b65de9a3c0f75687df679e8925d9b2e9
legacy3 <20936924+legacy3@users.noreply.github.com>
2025-12-07 23:49:51 +0100

    feat: add loader module for spell loading with progress tracking

--- apps/portal/src/lib/simulation/index.ts
+++ apps/portal/src/lib/simulation/index.ts
@@ -1,4 +1,5 @@
 export * from "./types";
 export * from "./runtime";
 export * from "./rotation-utils";
+export * from "./loader";
 export { getRotation, listRotations, BeastMasteryRotation } from "./rotations";
