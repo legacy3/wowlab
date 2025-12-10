231c75eca8d2d309d90daa753006c28ab02baa17
legacy3 <20936924+legacy3@users.noreply.github.com>
2025-12-10 03:50:02 +0100

    refactor: remove deprecated Markdown components and reorganize exports

--- apps/portal/src/components/docs/index.ts
+++ apps/portal/src/components/docs/index.ts
@@ -1,2 +1,8 @@
-export * from "./elements";
-export { Markdown } from "./markdown";
+export { MdHeading, MdH1, MdH2, MdH3, MdH4 } from "./md-heading";
+export { MdParagraph, MdLead, MdMuted, MdBlockquote } from "./md-text";
+export { MdUl, MdOl, MdLi } from "./md-list";
+export { MdLink } from "./md-link";
+export { MdTable, MdThead, MdTbody, MdTr, MdTh, MdTd } from "./md-table";
+export { MdInlineCode, MdPre, MdCode } from "./md-code";
+export { MdImage } from "./md-image";
+export { MdMermaid } from "./md-mermaid";
