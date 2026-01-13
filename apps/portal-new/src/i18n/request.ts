e9bf03675a70c67358a9dd38c255e0204c72baf2
legacy3 <20936924+legacy3@users.noreply.github.com>
2026-01-13 02:20:19 +0100

    feat: simplify message loading and restructure localization files

--- apps/portal-new/src/i18n/request.ts
+++ apps/portal-new/src/i18n/request.ts
@@ -2,24 +2,13 @@ import { getRequestConfig } from "next-intl/server";
 
 import { routing } from "./routing";
 
-const namespaces = ["common", "home"] as const;
-
-async function loadMessages(locale: string) {
-  const messages = await Promise.all(
-    namespaces.map((ns) =>
-      import(`../messages/${locale}/${ns}.json`).then((m) => m.default),
-    ),
-  );
-
-  return Object.assign({}, ...messages);
-}
-
 export default getRequestConfig(async ({ requestLocale }) => {
   const locale = (await requestLocale) ?? routing.defaultLocale;
+  const messages = (await import(`../messages/${locale}.json`)).default;
 
   return {
     locale,
-    messages: await loadMessages(locale),
+    messages,
     timeZone: "UTC",
   };
 });
